d4d8c581ff4d4e3e3a941e26846160d592737069
2b0f7bbd <est@localhost.localdomain>
2020-01-05 21:41:17 +0500

    Updates

---
+++ precache-manifest.7eb607db6397b435c058cb19a544afb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee4dcdbe15a6eca90fcaade3313cd8f",
+    "url": "/input_react_app/index.html"
+  },
+  {
+    "revision": "e35ba5a54994090d9fc0",
+    "url": "/input_react_app/static/css/main.943e8beb.chunk.css"
+  },
+  {
+    "revision": "eaa605eebceb1c66db32",
+    "url": "/input_react_app/static/js/2.faf33838.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/input_react_app/static/js/2.faf33838.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e35ba5a54994090d9fc0",
+    "url": "/input_react_app/static/js/main.115bd99d.chunk.js"
+  },
+  {
+    "revision": "1c5dfc24f9e0989f46cf",
+    "url": "/input_react_app/static/js/runtime-main.bbaea922.js"
+  }
+]);
